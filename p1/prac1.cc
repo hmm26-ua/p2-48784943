d2c23cf5e8010c90470604ab33063a4c5ba9857e
hmm26-ua <hectormoralmorales22@gmail.com>
2022-02-07 14:42:46 +0100

    Progreso hecho en clase 7febrero

--- p1/prac1.cc
+++ p1/prac1.cc
@@ -2,3 +2,90 @@
 // DNI: 48784943B
 // Nombre: HECTOR MORAL MORALES
 
+#include <iostream>
+#include <cstdlib> // Para rand(), srand() y atoi()
+
+using namespace std;
+
+const int KNAME=32;
+const int KENEMIES=5;
+const int KPOINTS=200;
+const int KDICE=20; // Número de caras del dado
+
+struct Core{
+  int attack;
+  int defense;
+  int hp;
+};
+
+enum Breed{
+  AXOLOTL,
+  TROLL,
+  ORC,
+  HELLHOUND,
+  DRAGON
+};
+
+struct Enemy{
+  Breed name;
+  Core features;
+};
+
+struct Hero{
+  char name[KNAME];
+  Core features;
+  bool special;
+  int runaways;
+  int exp;
+  int kills[KENEMIES];
+};
+
+int rollDice(){
+  return rand()%KDICE+1;
+}
+
+Hero createHero(){
+    Hero, hero;
+    do{
+        cout<<"Introduce el nombre de tu heroe o heroina: "<< endl;
+        cin.getline(name, KNAME-1);¨
+        if(iswalpha(name[0]))
+            else(iswprint(name) = False);
+            else if(iswprint(name) = True);
+            cout<<"Error"<< endl;
+    }
+
+    return hero;
+}
+
+Enemy createEnemy(){
+}
+
+void fight(Hero &hero,Enemy &enemy){
+}
+
+void report(const Hero &hero){
+}
+
+void showMenu(){
+  cout << "[Options]" << endl
+       << "1- Fight" << endl
+       << "2- Run away" << endl
+       << "3- Special" << endl 
+       << "4- Report" << endl
+       << "q- Quit" << endl
+       << "Option: ";
+}
+
+int main(int argc,char *argv[]){
+  if(argc!=2){ // Si los parámetros no son correctos, el programa termina inmediatamente
+    cout << "Usage: " << argv[0] << " <seed>" << endl;
+  }
+  else{
+    srand(atoi(argv[1])); // Introducimos la semilla para generar números aleatorios
+    
+    // Aquí vendrá todo tu código del "main"...
+  }
+}
+
+
